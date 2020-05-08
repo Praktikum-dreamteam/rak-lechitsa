4ab1f9ee65ed48bc6c6657399d8b0a92282bfc21
Ilya <Ilya_11_11@mail.ru>
2020-05-08 20:37:39 +0300

    basicStracture

---
+++ components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <header class="header">
+    <h2 class="header__title">
+      Проект Благотворительного Фонда Константина Хабенского
+    </h2>
+    <div class="header__links">
+      <header-nav />
+      <header-btn class="header__button">Рассказать историю</header-btn>
+    </div>
+  </header>
+</template>
+
+<script>
+import Button from '~/components/ui/Button';
+import Nav from '@/components/Nav';
+export default {
+  components: {
+    'header-btn': Button,
+    'header-nav': Nav,
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1440px;
+  width: 100%;
+  padding: 18px 3%;
+  border-bottom: 1px solid #e8e8e8;
+}
+.header__title {
+  font-size: 16px;
+  line-height: 20px;
+  font-weight: 600;
+  max-width: 340px;
+}
+.header__links {
+  display: flex;
+  align-items: center;
+}
+.header__button {
+  padding: 0;
+  margin-left: 40px;
+  font-size: 18px;
+  line-height: 24px;
+  background-color: transparent;
+  border: none;
+}
+.header__button:hover {
+  cursor: pointer;
+}
+</style>
---
+++ components/Nav.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav class="nav">
+    <ul class="nav__lists">
+      <nuxt-link v-if="$route.path !== '/'" to="/" class="nav__link"
+        >Главная</nuxt-link
+      >
+      <nuxt-link v-else to="/" class="nav__link nav__link_underline"
+        >Главная</nuxt-link
+      >
+      <nuxt-link
+        v-if="$route.path !== '/stories'"
+        to="/stories"
+        class="nav__link"
+        >Истории</nuxt-link
+      >
+      <nuxt-link v-else to="/stories" class="nav__link nav__link_underline"
+        >Истории</nuxt-link
+      >
+    </ul>
+  </nav>
+</template>
+
+<script></script>
+
+<style scoped>
+.nav__lists {
+  display: flex;
+  list-style: none;
+  padding: 0;
+}
+.nav__link {
+  text-decoration: none;
+  color: black;
+  margin-left: 40px;
+}
+.nav__link:first-child {
+  margin: 0;
+}
+.nav__link_underline {
+  text-decoration: underline;
+}
+</style>
