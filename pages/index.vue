6f544834b64ac9b0acba94dcbc46f884d0cb6757
SidorovN <57040459+SidorovN@users.noreply.github.com>
2020-05-09 22:30:30 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,76 @@
 <template>
   <div class="container">
-    <story-card />
+    <div class="test">
+      <Cover />
+      <Video />
+      <Slogan />
+      <History />
+      <Slogan2 />
+      <Instagram />
+      <Forma />
+      <Statistics />
+      <Info />
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Logo from '~/components/Logo.vue';
+import Cover from '@/components/Cover';
+import Video from '@/components/Video';
+import Slogan from '@/components/Slogan';
+import History from '@/components/History';
+import Slogan2 from '@/components/Slogan2';
+import Instagram from '@/components/Instagram';
+import Forma from '@/components/Forma';
+import Statistics from '@/components/Statistics';
+import Info from '@/components/Info';
+export default {
+  components: {
+    Logo,
+    Cover,
+    Video,
+    Slogan,
+    History,
+    Slogan2,
+    Instagram,
+    Forma,
+    Statistics,
+    Info,
+  },
+};
 </script>
 
-<style></style>
+<style>
+.container {
+  margin: 0 auto;
+  width: 1440px;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+.links {
+  padding-top: 15px;
+}
+.test {
+  width: 100%;
+}
+</style>
