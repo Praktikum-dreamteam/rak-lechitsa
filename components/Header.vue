bb0d47dd0ba0c39b99d25b841047b7ad73b5ee85
Ilya <Ilya_11_11@mail.ru>
2020-05-12 20:51:53 +0300

    addH1

--- components/Header.vue
+++ components/Header.vue
@@ -40,36 +40,37 @@
           <line y1="1.5" x2="32" y2="1.5" stroke="black" stroke-width="3" />
         </svg>
       </menu-btn>
-    </div>
-    <close-menu
-      v-if="isMenuOpen"
-      @btn-click="[addMenuBtn(), $emit('closeMenuClick')]"
-    >
-      <svg
-        width="26"
-        height="26"
-        viewBox="0 0 26 26"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
+      <close-menu
+        theme="close"
+        v-if="isMenuOpen"
+        @btn-click="[addMenuBtn(), $emit('closeMenuClick')]"
       >
-        <line
-          x1="1.62684"
-          y1="24.2528"
-          x2="24.2543"
-          y2="1.6254"
-          stroke="black"
-          stroke-width="3"
-        />
-        <line
-          x1="24.2557"
-          y1="24.3741"
-          x2="1.62833"
-          y2="1.74672"
-          stroke="black"
-          stroke-width="3"
-        />
-      </svg>
-    </close-menu>
+        <svg
+          width="26"
+          height="26"
+          viewBox="0 0 26 26"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <line
+            x1="1.62684"
+            y1="24.2528"
+            x2="24.2543"
+            y2="1.6254"
+            stroke="black"
+            stroke-width="3"
+          />
+          <line
+            x1="24.2557"
+            y1="24.3741"
+            x2="1.62833"
+            y2="1.74672"
+            stroke="black"
+            stroke-width="3"
+          />
+        </svg>
+      </close-menu>
+    </div>
   </header>
 </template>
 
@@ -129,9 +130,6 @@ export default {
 }
 .svg {
   margin-bottom: 4px;
-}
-.btn_theme_close {
-  display: none;
 }
 @media screen and (max-width: 1280px) {
   .header {
--- pages/stories/index.vue
+++ pages/stories/index.vue
@@ -1,4 +1,8 @@
-<template> </template>
+<template>
+  <h1 class="page__section">
+    Здесь будет страница с историями
+  </h1>
+</template>
 
 <script></script>
 
