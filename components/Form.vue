851db050be84bfe07486c71d352adcc2c2f094ea
SidorovN <57040459+SidorovN@users.noreply.github.com>
2020-05-12 12:39:03 +0300

    [fix]Form

--- components/Form.vue
+++ components/Form.vue
@@ -1,54 +1,96 @@
 <template>
-  <div>
-    <button
-      v-bind:disabled="isDisabledActive"
-      :type="type"
-      @click.prevent="$emit('btn-click')"
-      :class="['btn', `btn_theme_${theme}`, { btn_disabled: isDisabledActive }]"
-    >
-      <slot></slot>
-    </button>
-  </div>
+  <form @click="test" class="form">
+    <customLabel :labelText="formQuestion" :description="description" />
+    <inputForm
+      :valueInput="currentAnswer"
+      v-model="currentAnswer"
+      @input="$emit('answerInput', $event)"
+    />
+    <div class="form__buttons">
+      <btn
+        type="button"
+        :isDisabledActive="isDisabledBackButton"
+        @btn-click="$emit('clickBack')"
+        class="form__button"
+        theme="grey"
+        >Назад</btn
+      >
+      <btn
+        type="submit"
+        @btn-click="$emit('clickNext')"
+        class="form__button"
+        theme="violet"
+        >{{ isLast ? 'Отправить' : 'Далее' }}</btn
+      >
+      <p v-if="isLast" class="form__personal-data-agreement">
+        Нажимая на кнопку «отправить», вы даете согласие на
+        <nuxt-link class="form__link" to="/stories"
+          >обработку персональных данных</nuxt-link
+        >
+      </p>
+    </div>
+  </form>
 </template>
 
 <script>
+import Button from '@/components/ui/Button';
+import Input from '@/components/ui/Input';
+import Label from '@/components/ui/Label';
 export default {
-  props: ['theme', 'type', 'isDisabledActive'],
+  props: [
+    'formQuestion',
+    'description',
+    'answer',
+    'isLast',
+    'isDisabledBackButton',
+  ],
+  components: {
+    btn: Button,
+    inputForm: Input,
+    customLabel: Label,
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    currentAnswer: {
+      get() {
+        return this.answer;
+      },
+      set(value) {},
+    },
+  },
+  methods: {
+    test() {},
+  },
 };
 </script>
 
 <style scoped>
-.btn {
-  font-family: 'Inter', sans-serif;
-  font-size: 16px;
-  line-height: 24px;
-  background: none;
-  border: none;
-  cursor: pointer;
+.form {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-top: 40px;
 }
-
-.btn:focus {
-  outline: none;
+.form__buttons {
+  padding-top: 100px;
+  display: flex;
+  align-items: center;
 }
-.btn_theme_violet {
-  color: #fff;
-  background: #613a93;
-  line-height: 19px;
-  padding: 16px 80px;
+.form__button {
+  margin-right: 30px;
 }
-.btn_theme_grey {
+.form__button:last-child {
+  margin-right: 0;
+}
+.form__personal-data-agreement {
+  max-width: 378px;
+  font-size: 14px;
+  line-height: 17px;
   color: #666;
-  line-height: 19px;
 }
-
-.btn_disabled {
-  opacity: 0.5;
-  cursor: not-allowed;
-}
-
-@media screen and (max-width: 768px) {
-  .btn_theme_violet {
-    padding: 14px 40px;
-  }
+.form__link {
+  color: #666;
 }
 </style>
